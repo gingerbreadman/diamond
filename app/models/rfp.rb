16cf9a92d90e4b6e5fbab28c74bb992550e85ea3
Rahim Ladak <rahim.ladak@gmail.com>
2013-11-21 11:06:08 -0600

    Cleand up some UI

--- app/models/rfp.rb
+++ app/models/rfp.rb
@@ -2,4 +2,10 @@ class Rfp < ActiveRecord::Base
   has_many :matches
   has_many :offers
   belongs_to :user
+
+  def full_diamond_trait
+    return "#{max_price}  #{carat}  #{shape} #{color}  #{cut}  #{clarity}"
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :rfps
   has_many :offers
+
+  def full_name
+    return "#{first_name} #{last_name}"
+  end
+
 end
